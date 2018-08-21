c5afeb0f30d621c9dc38bf93d1d059386bb37f4e
ivanwakeup <ivan9miller@gmail.com>
2018-08-20 20:09:58 -0600

    add django-import-export

--- pycrawl3/settings/common.py
+++ pycrawl3/settings/common.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pycrawl3',
+    'import_export',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/templates/'
+STATIC_ROOT = 'static'
