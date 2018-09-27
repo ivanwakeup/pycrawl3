27bc4f4a2258641e3ebd14e678aaf1aa7ea33e28
ivanwakeup <ivan9miller@gmail.com>
2018-09-27 08:49:04 -0600

    wip blogger branch

--- pycrawl3/models.py
+++ pycrawl3/models.py
@@ -2,17 +2,29 @@ from django.db import models
 
 
 class Email(models.Model):
-    email_address = models.CharField("Email Address", primary_key=True, max_length=255, null=False)
-    seed_url = models.CharField("Seed Url", max_length=255, null=True)
-    from_url = models.CharField("Url Extracted From", max_length=255, null=True)
+    email_address = models.CharField("Email Address", max_length=1000, primary_key=True, null=False)
+    seed_url = models.CharField("Seed Url", max_length=1000, null=True)
+    from_url = models.CharField("Url Extracted From", max_length=1000, null=True)
     tier = models.IntegerField()
-    createdTime = models.DateTimeField("Created Time", auto_now_add=True)
-    modifiedTime = models.DateTimeField("Modified Time", auto_now=True, null=True)
+    modified_count = models.IntegerField(default=0)
+    created_time = models.DateTimeField("Created Time", auto_now_add=True)
+    modified_time = models.DateTimeField("Modified Time", auto_now=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.modified_count += 1
+        return super(Email, self).save(*args, **kwargs)
 
 
 class Seed(models.Model):
-    url = models.CharField("Url", primary_key=True, max_length=255, null=False)
+    url = models.CharField("Url", primary_key=True, max_length=1000, null=False)
     crawled = models.BooleanField("Url Crawled")
     crawl_count = models.IntegerField("Crawl Count", default=0)
-    createdTime = models.DateTimeField("Created Time", auto_now_add=True)
-    modifiedTime = models.DateTimeField("Updated Time", auto_now=True, null=True)
+    created_time = models.DateTimeField("Created Time", auto_now_add=True)
+    modified_time = models.DateTimeField("Updated Time", auto_now=True, null=True)
+
+
+class Blogger(models.Model):
+    email = models.ForeignKey(Email, on_delete=models.CASCADE)
+    tags = models.CharField("Tags", max_length=1000, null=True)
+    created_time = models.DateTimeField("Created Time", auto_now_add=True)
+    modified_time = models.DateTimeField("Modified Time", auto_now=True)
