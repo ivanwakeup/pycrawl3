f6d45d65eba0566b8d40f09245456a1c46761e26
ivanwakeup <ivan9miller@gmail.com>
2018-10-17 08:27:11 -0600

    yep

--- pycrawl3/nlp/similar_words.py
+++ pycrawl3/nlp/similar_words.py
@@ -1,5 +1,6 @@
 import spacy
 from sense2vec import Sense2VecComponent
+from pycrawl3.utils.logger import log
 
 nlp = spacy.load('en')
 s2v = Sense2VecComponent('/Users/ivanwakeup/data/reddit_vectors-1.1.0')
@@ -8,9 +9,15 @@ nlp.add_pipe(s2v)
 
 def get_similar_words(word):
     doc = nlp(u"{}".format(word))
-    print(doc[0])
+    log.info("getting similar words for {}...".format(word))
     assert doc[0].text == u"{}".format(word)
-    freq = doc[0]._.s2v_freq
-    vector = doc[0]._.s2v_vec
+    #freq = doc[0]._.s2v_freq
+    #vector = doc[0]._.s2v_vec
     most_similar = doc[0]._.s2v_most_similar(15)
-    print(most_similar)
+
+    res = []
+    for similar in most_similar:
+        res.append(similar[0][0])
+
+    log.info("similar words for {} are {}...".format(word, res))
+    return ",".join(res)
--- pycrawl3/scripts/load_csv_seeds.py
+++ pycrawl3/scripts/load_csv_seeds.py
@@ -14,28 +14,42 @@ from google import google
 import csv
 from pycrawl3.persistence.persistence import SeedDelegate
 from pycrawl3.nlp.similar_words import get_similar_words
+from pycrawl3.utils.logger import log
+import time
 
 
 def load_csv_seeds(file):
+    seed_package = []
     with open(file, newline='\n') as csv_file:
         reader = csv.reader(csv_file, delimiter=",")
         for row in reader:
             category = row[0]
             topic = row[1]
             search_term = row[2]
+            seed_package.append((category, topic, search_term))
 
-            weighted_terms = get_similar_words(topic)
+    for p in seed_package:
+        weighted_terms = get_similar_words(p[1])
 
-            results = google.search(search_term, 1)
-            for result in results:
-                seed = Seed(
-                    url=result.link,
-                    category=category,
-                    search_term=search_term,
-                    weighted_terms=weighted_terms,
-                    crawled=False
-                )
-                SeedDelegate.add_or_update_seed(seed)
+        log.info("waiting 60 seconds before making next google search....")
+        time.sleep(60)
+        log.info("trying to search google for {}".format(p[2]))
+
+        try:
+            results = google.search(p[2], 1)
+        except Exception as e:
+            log.error("There was an issue searching google: {}".format(e))
+            continue
+
+        for result in results:
+            seed = Seed(
+                url=result.link,
+                category=p[0],
+                search_term=p[2],
+                weighted_terms=weighted_terms,
+                crawled=False
+            )
+            SeedDelegate.add_or_update_seed(seed)
 
 
 if __name__ == "__main__":
